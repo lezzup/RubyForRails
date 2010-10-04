f48b2f77a0a0cd4c3ea2bc07b5463a16218632a3
Nele Vanderhaegen <nele@stijn-van-vreckems-imac.local>
2010-10-04 17:31:13 +0200

    2.3.4 and 2.3.5 of Ruby for rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+    map.connect '', :controller => "main", :action => "welcome"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -40,4 +41,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
 end
